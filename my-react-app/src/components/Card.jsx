38f5df719ea249a7a56dd8c2092407287e5deb41
RodolpheACHY <rod23_reseaux@yahoo.fr>
2025-03-04 02:50:52 +0100

    add feat card container OK  + correction bug background Hero --> ok  et bug webkit-text-stroke hero h1

--- my-react-app/src/components/Card.jsx
+++ my-react-app/src/components/Card.jsx
@@ -1,18 +1,16 @@
 import React from "react";
+import styles from "./card.module.scss";
 
-const Card = ({ country }) => {
-  console.log(country);
+const Card = ({ location }) => {
+  console.log(location);
   return (
-    <li className="card">
+    <li className={styles.card}>
       <img
-        src={country.flags.svg}
-        alt={"drapeau " + country.translations.fra.common}
+        className={styles.card__img}
+        src={location.cover}
+        alt={"photo location " + location.title}
       />
-      <div className="infos">
-        <h2>{country.translations.fra.common}</h2>
-        <h4>{country.capital}</h4>
-        <p>Pop. {country.population.toLocaleString()}</p>
-      </div>
+      <h2 className={styles.card__title}>{location.title}</h2>
     </li>
   );
 };
--- my-react-app/src/components/Locations.jsx
+++ my-react-app/src/components/Locations.jsx
@@ -1,24 +1,30 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import Card from "./Card";
+import styles from "./locations.module.scss"; // pour ajouter un style au container
+import locationsData from "../data/logements.json"; // Vérifie que le JSON est bien importé
 
 const Locations = () => {
   const [locations, setLocations] = useState([]);
 
-  useEffect(() => {
+  /*useEffect(() => {
     axios
       .get("/logements.json")
       .then((response) => setLocations(response.data))
       .catch((error) =>
         console.error("Erreur lors de la récupération :", error)
       );
+  }, []); */
+
+  useEffect(() => {
+    setLocations(locationsData); // Charge les données
   }, []);
 
   return (
     <div>
-      <h1>Liste des locations</h1>
-      <ul>
+      <ul className={styles.locations}>
         {locations.map((location) => (
-          <li key={location.id}>{location.title}</li>
+          <Card key={location.id} location={location} /> // pour passer `location` en prop
         ))}
       </ul>
     </div>
